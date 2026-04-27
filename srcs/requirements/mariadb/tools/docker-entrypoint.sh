12d27675c57ed781f8b7ebc651d43955f0d1fe03
Fernando Silva <fernandowonder123@gmail.com>
2026-04-27 17:08:59 +0100

    stable

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-WORDPRESS_DB_PASSWORD=$(cat /run/secrets/db_password.txt)
+WORDPRESS_DB_PASSWORD=$(cat /run/secrets/db_password)
 #ROOT_PASSWORD=$(cat /run/secrets/db_root_password.txt)
 
 if [ -z "$(ls -A /var/lib/mysql)" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-    sed "s/\[WORDPRESS_DB_PASSWORD\]/$WORDPRESS_DB_PASSWORD/g" /tmp/init.sql \
+fi
+sed "s/\[WORDPRESS_DB_PASSWORD\]/$WORDPRESS_DB_PASSWORD/g" /tmp/init.sql \
         > /tmp/init_ready.sql
 
-    exec mysqld --user=mysql --datadir=/var/lib/mysql --init-file=/tmp/init_ready.sql
-fi
-
-exec "$@"
+exec mysqld --user=mysql --datadir=/var/lib/mysql --init-file=/tmp/init_ready.sql --bind-address=0.0.0.0
